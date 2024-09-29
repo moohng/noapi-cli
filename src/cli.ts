c8843be5c55cfa8ec71a8f3aa6ecc6c02ec5fe3b
mohong <mohong@zmn.cn>
2024-09-29 14:30:13 +0800

    搜索指令

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,7 @@
 import path from 'path';
 import { program } from 'commander';
 import * as readline from 'readline/promises';
-import { createNoApi, createConfigFile, getConfigPath, mergeConfig, appendToFile, checkExists, writeToFile, writeToIndexFile } from '@noapi/core';
+import { createNoApi, createConfigFile, getConfigPath, mergeConfig, appendToFile, checkExists, writeToFile, writeToIndexFile, loadConfig } from '@noapi/core';
 
 /**
  * 报错并退出
@@ -22,20 +22,17 @@ function exitWithError(...messages: string[]) {
 program
   .version('1.0.0')
   .description(
-    '欢迎使用 NoAPI，使用 npx @noapi/cli api url1,url2... 立即体验！\n\
-如果生成的api方法或类型文件有问题，建议使用-p参数查看接口相关信息，然后使用def命令手动生成类型定义。\n\
-swUrl、cookie等相关参数建议写在配置文件noapi.config.js中。'
+    '欢迎使用 NoAPI ！\n\
+1. 使用 npx @noapi/cli init 初始化配置文件；\n\
+2. 使用 npx @noapi/cli api url1,url2... 开始体验。\n'
   );
 
 // api命令
 program
-  .command('api [urls]', { isDefault: true }) // <urls...> 可以解析成一个数组
+  .command('api <urls>', { isDefault: true }) // <urls...> 可以解析成一个数组
   .description('生成api函数，url路径不能以/开头，多个url用逗号分隔')
-  .option('-m, --method [method]', '指定接口方法，不指定可能会生成多个api函数')
+  .option('-m, --method <method>', '指定接口方法，不指定可能会生成多个api函数')
   .option('-d, --only-def [onlyDef]', '只生成类型定义文件')
-  .option('-u, --swag-url <swagUrl>', '指定swagger文档地址')
-  .option('-c, --cookie <cookie>', 'url的授权cookie')
-  .option('-l, --list [showList]', '查询api接口，支持搜索')
   .action(async (urls, options) => {
     const execStart = performance.now();
     console.log('开始运行...');
@@ -45,37 +42,20 @@ program
     const { onlyDef = false, method, ...otherOptions } = options;
     const { swagFile, apiBase = './src/api', defBase, fileHeader, exportFromIndex = true, ...config } = mergeConfig(otherOptions);
     const swFilePath = path.resolve(swagFile || path.join(apiBase, 'noapi-swagger-doc.json'));
-    if (!options.swagUrl) {
-      try {
-        config.swagJson = require(swFilePath);
-      } catch (error) {
-        if (!config.swagUrl) {
-          exitWithError('获取swagger文档失败，请提供本地或在线文档！');
-        }
+
+    try {
+      config.swagSource = require(swFilePath);
+    } catch (error) {
+      if (!config.swagSource) {
+        exitWithError('获取swagger文档失败，请提供本地或在线文档！');
       }
     }
 
     const noapi = createNoApi(config);
 
-    if (!noapi.swagJson) {
+    if (!noapi.getSwagJson()) {
       const result = await noapi.fetchDataSource();
       await writeToFile(swFilePath, JSON.stringify(result, null, 2));
-    }
-
-    if (options.list) {
-      let apis = await noapi.listApi();
-      if (typeof options.list === 'string') {
-        const keyword = options.list.trim();
-        apis = apis.filter((api) => api.url.includes(keyword) || api.summary.includes(keyword) || api.tag?.includes(keyword));
-      }
-      if (!apis?.length) {
-        console.log('没有找到api！');
-      } else {
-        console.log(apis);
-        console.log(`共找到 ${apis.length} 条 api`);
-      }
-      console.log(`运行耗时 ${(performance.now() - execStart).toFixed(2)}ms`);
-      return;
     }
 
     urls = urls?.split(',').map((url: string) => `/${url}`);
@@ -110,6 +90,47 @@ program
     console.log(`运行耗时 ${(performance.now() - execStart).toFixed(2)}ms`);
   });
 
+// 搜索 api 命令
+program
+  .command('search <keyword>')
+  .description('搜索api接口')
+  .action(async (keyword) => {
+    const execStart = performance.now();
+    console.log('开始运行...');
+    console.log('搜索关键字：', keyword);
+
+    const { swagFile, apiBase = './src/api', ...config } = loadConfig();
+    const swFilePath = path.resolve(swagFile || path.join(apiBase, 'noapi-swagger-doc.json'));
+
+    try {
+      config.swagSource = require(swFilePath);
+    } catch (error) {
+      if (!config.swagSource) {
+        exitWithError('获取swagger文档失败，请提供本地或在线文档！');
+      }
+    }
+
+    const noapi = createNoApi(config);
+
+    if (!noapi.getSwagJson()) {
+      const result = await noapi.fetchDataSource();
+      await writeToFile(swFilePath, JSON.stringify(result, null, 2));
+    }
+
+    let apis = await noapi.listApi();
+    if (typeof keyword === 'string') {
+      keyword = keyword.trim();
+      apis = apis.filter((api) => api.url.includes(keyword) || api.summary.includes(keyword) || api.tag?.includes(keyword));
+    }
+    if (!apis?.length) {
+      console.log('没有找到api！');
+    } else {
+      console.log(apis);
+      console.log(`共找到 ${apis.length} 条 api`);
+    }
+    console.log(`运行耗时 ${(performance.now() - execStart).toFixed(2)}ms`);
+  });
+
 // 初始化配置命令
 program
   .command('init')
@@ -132,19 +153,17 @@ program
 
     const configFile = await createConfigFile(swagUrl, configFilePath);
 
-    console.log(`配置文件${configFile}已生成，可自定义配置。`);
+    console.log(`配置文件${configFile}已生成，可自定义配置信息。`);
   });
 
 // 更新文档
 program
   .command('update')
   .description('更新swagger文档')
-  .option('-u, --swag-url <swagUrl>', '指定swagger文档地址')
-  .option('-c, --cookie <cookie>', 'url的授权cookie')
-  .action(async (options) => {
+  .action(async () => {
     console.log('开始运行...');
 
-    const config = mergeConfig(options);
+    const config = loadConfig();
 
     const noapi = createNoApi(config);
 
